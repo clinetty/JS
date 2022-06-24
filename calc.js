05f2aa08033bc094074faa1a4ee121dd9555ea57
Clinton <clintogechi@gmail.com>
2022-06-24 16:08:30 +0300

    Added calc.js

---
+++ calc.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+    return a + b;
+}
+const subtract = (a, b) => {
+    return a - b;
+}
+const multiply = (a, b) => {
+    return a * b;
+}
+const division = (a, b) => {
+    return a / b;
+}
+
+console.log(add(10, 5)); //15
+console.log(subtract(10, 5)); //5
+console.log(multiply(10, 5)); //50
+console.log(division(10, 5)); //2
